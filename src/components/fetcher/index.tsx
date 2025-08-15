8b2ffce5b820b9f18fa2fb86fef6c8c279ed1ccb
Jeremy Pittard <jeremy@jpittard.net>
2025-08-15 10:19:21 +0800

    Update index.tsx
    changed to use form submit so hitting enter works

--- src/components/fetcher/index.tsx
+++ src/components/fetcher/index.tsx
@@ -30,21 +30,20 @@ const FetchForm = () => {
 
   return (
     <section className="max-w-lg mx-auto">
-      <div className="p-4">
-        <div className="flex flex-col">
-          <label htmlFor="name">Enter an album title:</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            onChange={(e) => setAlbumTitle(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && handleSubmit}
-          />
-          <button type="button" onClick={handleSubmit}>
-            Go!
-          </button>
+      <form onSubmit={handleSubmit}>
+        <div className="p-4">
+          <div className="flex flex-col">
+            <label htmlFor="name">Enter an album title:</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              onChange={(e) => setAlbumTitle(e.target.value)}
+            />
+            <button type="submit">Go!</button>
+          </div>
         </div>
-      </div>
+      </form>
       <div className="flex flex-col gap-4">
         {isLoading ? <h2>doing the thing</h2> : null}
         {albums?.map((album: SimplifiedAlbum) => (
